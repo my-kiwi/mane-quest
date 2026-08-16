dff6d44a7ec5c3fd70bf746bff384453da6aac5b
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-08-16 13:58:28 +0000

    refactor: use proportional heigth/width

--- src/unicorn.ts
+++ src/unicorn.ts
@@ -1,18 +1,27 @@
 import { Unicorn } from './types';
 import { canvas } from './canvas';
+import { getMinHeightWidth, getWidth, getHeight } from './utils';
 
 // Unicorn properties and state
 export const unicorn: Unicorn = {
   image: new Image(),
   x: 0, // Will be set to center in initialize
   y: 0,
-  width: 80,
-  height: 80,
-  speed: 5,
+  get width() {
+    return getMinHeightWidth() * 0.1; // 10% of the smaller dimension
+  },
+  get height() {
+    return getMinHeightWidth() * 0.1; // 10% of the smaller dimension
+  },
+  get speed() {
+    return getWidth() * 0.005;
+  },
   direction: -1, // -1 for left, 1 for right
   isJumping: false,
   velocityY: 0,
-  jumpStrength: 13,
+  get jumpStrength() {
+    return getHeight() * 0.02;
+  },
   gravity: 0.7,
   rotation: 0,
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
 // Utility functions
 export const clamp = (value: number, min: number, max: number) =>
   Math.min(Math.max(value, min), max);
+
+export const getHeight = () => {
+  return window.innerHeight;
+};
+
+export const getWidth = () => {
+  return window.innerWidth;
+};
+
+export const getMinHeightWidth = () => {
+  return Math.min(getHeight(), getWidth());
+};
