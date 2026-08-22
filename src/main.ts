c39343824d0bb3aaa71aace3dc85a58355d0f4ef
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-08-22 19:28:53 +0000

    feat: start music

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { initializeCanvas } from './canvas';
 import { initializeUnicorn } from './unicorn';
 import { initializeInput } from './input';
 import { startGameLoop } from './gameLoop';
+import Music from './music';
 
 // Initialize all systems
 initializeCanvas();
@@ -13,3 +14,11 @@ initializeInput();
 
 // Start the game loop
 startGameLoop();
+
+function startMusic(){
+    window.removeEventListener('pointerdown',startMusic);
+    Music.play('overworld'); 
+}
+
+// start music
+window.addEventListener('pointerdown', startMusic);
