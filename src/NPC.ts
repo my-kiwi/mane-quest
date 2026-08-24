5e3e218b29bcf3aa6ac91fb925687bebcd1773c3
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-08-24 19:18:20 +0000

    feat: change dialog after death

--- src/NPC.ts
+++ src/NPC.ts
@@ -15,17 +15,43 @@ export const sageNpc = {
     maneColor: '#A7AFB7',
   },
   get dialogue() {
-    return [
-      { line: 'Welcome, dear unicorn.' },
-      { line: 'I am the wise sage of this land.' },
-      { line: '...' },
-      { who: unicorn, line: '...' },
-      { line: '...' },
-      { who: unicorn, line: '...' },
-      { line: "Look, I don't actually have any wisdom. I just stand here." },
-      { line: 'What do you want, a goodbye kiss?' },
-    ];
-  }
+    if (unicorn.deaths === 0) {
+      return [
+        { line: 'Welcome, dear unicorn.' },
+        { line: 'I am the wise sage of this land.' },
+        { line: '...' },
+        { who: unicorn, line: '...' },
+        { line: '...' },
+        { who: unicorn, line: '...' },
+        { line: "Look, I don't actually have any wisdom. I just stand here." },
+        { line: 'What do you want, a goodbye kiss?' },
+      ];
+    } else if (unicorn.deaths === 1) {
+      return [
+        { line: 'Welcome, dear unicorn.' },
+        { line: 'I am the wise sage of this land.' },
+        { line: 'I see you have met the challenges of this land.' },
+        { line: 'Anyways, why did you jump into the void?' },
+        { line: 'Passage is that way' },
+        { who: unicorn, line: '...' },
+        { line: '...' },
+        { who: unicorn, line: '...' },
+        { line: "Look, I don't actually have any wisdom. I just stand here." },
+        { line: 'What do you want, a goodbye kiss?' },
+      ];
+    } else {
+      return [
+        { line: 'Ah, you have returned once again.' },
+        { line: 'I see you have faced many trials and tribulations.' },
+        { line: '...' },
+        { who: unicorn, line: '...' },
+        { line: '...' },
+        { who: unicorn, line: '...' },
+        { line: "Look, I don't actually have any wisdom. I just stand here." },
+        { line: 'What do you want, a goodbye kiss?' },
+      ];
+    }
+  },
 };
 sageNpc.image.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(
   createUnicornSvg(sageNpc.colors)
