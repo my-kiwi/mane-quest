6dee7460bfad2693b69630409ebca0b11f22e8c9
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-09-09 12:28:40 +0000

    fix: remove collisions with NPC for easier movements

--- src/levels/levelGeometry.ts
+++ src/levels/levelGeometry.ts
@@ -13,8 +13,7 @@ export function isSolid(tile: string | undefined): boolean {
   return (
     tile === TileType.GROUND ||
     tile === TileType.PLATFORM ||
-    tile === TileType.INVISIBLE_PLATFORM ||
-    tile === TileType.NPC
+    tile === TileType.INVISIBLE_PLATFORM
   );
 }
 
