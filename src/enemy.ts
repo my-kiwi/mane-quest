9982bbd1f9916d541723e4858e24cfeb2b4536f3
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-09-09 11:50:09 +0000

    refactor: simplify enemy svg

--- src/enemy.ts
+++ src/enemy.ts
@@ -39,30 +39,7 @@ export const firstEnemy = {
   },
 };
 
-const enemySvg = `<svg width="200" height="200" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-  <path d="M50,80 Q70,30 100,50 Q130,20 150,80 Q170,120 140,150 Q100,170 70,140 Q40,120 50,80"
-        fill="url(#rainbow)" stroke="black" stroke-width="2" />
-  <circle cx="90" cy="60" r="15" fill="white" stroke="black" stroke-width="2" />
-  <circle cx="95" cy="65" r="5" fill="black" />
-    <circle cx="120" cy="60" r="15" fill="white" stroke="black" stroke-width="2" />
-  <circle cx="120" cy="65" r="5" fill="black" />
-  <line x1="80" y1="140" x2="70" y2="180" stroke="black" stroke-width="3" />
-  <path d="M100,140 Q110,160 120,180" stroke="black" stroke-width="3" fill="none" />
-  <line x1="130" y1="140" x2="140" y2="160" stroke="black" stroke-width="3" />
-  <circle cx="140" cy="160" r="5" fill="pink" />
-  <path d="M70,100 Q90,120 110,100" stroke="black" stroke-width="2" fill="none" />
-  <defs>
-    <linearGradient id="rainbow" x1="0%" y1="0%" x2="100%" y2="100%">
-      <stop offset="0%" stop-color="red" />
-      <stop offset="25%" stop-color="orange" />
-      <stop offset="50%" stop-color="yellow" />
-      <stop offset="75%" stop-color="green" />
-      <stop offset="100%" stop-color="blue" />
-    </linearGradient>
-  </defs>
-</svg>`;
+const enemySvg = `<svg width="200" height="200" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"><path d="M50 80q20-50 50-30 30-30 50 30 20 40-10 70-40 20-70-10-30-20-20-60" fill="url(#a)" stroke="#000" stroke-width="2"/><circle cx="90" cy="60" r="15" fill="#fff" stroke="#000" stroke-width="2"/><circle cx="95" cy="65" r="5"/><circle cx="120" cy="60" r="15" fill="#fff" stroke="#000" stroke-width="2"/><circle cx="120" cy="65" r="5"/><path d="M70 100q20 20 40 0" stroke="#000" stroke-width="2" fill="none"/><defs><linearGradient id="a" x1="0%" y1="0%" x2="100%" y2="100%"><stop offset="0%" stop-color="red"/><stop offset="25%" stop-color="orange"/><stop offset="50%" stop-color="#ff0"/><stop offset="75%" stop-color="green"/><stop offset="100%" stop-color="#00f"/></linearGradient></defs></svg>`;
 
 firstEnemy.image.src = encodeSvg(enemySvg);
-firstEnemy.deadImage.src = encodeSvg(
-  enemySvg.replace('url(#rainbow)', 'black').replace('fill="pink"', 'fill="black"')
-);
+firstEnemy.deadImage.src = encodeSvg(enemySvg.replace('url(#a)', 'black'));
